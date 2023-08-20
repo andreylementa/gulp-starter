cf556be715b86f109d8cbc1f389723c9fc66a89f
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-08-20 16:54:58 +0300

    webp-css

--- task/css.js
+++ task/css.js
@@ -15,6 +15,7 @@ const rename = require("gulp-rename");
 const size = require("gulp-size");
 const shorthand = require("gulp-shorthand");
 const groupCssMediaQueries = require("gulp-group-css-media-queries");
+const webpCss = require("gulp-webp-css");
 
 // Обработка CSS
 const css = function () {
@@ -29,6 +30,7 @@ const css = function () {
     )
     .pipe(concat("main.css"))
     .pipe(cssimport())
+    .pipe(webpCss())
     .pipe(autoprefixer())
     .pipe(shorthand())
     .pipe(groupCssMediaQueries())
